6d6049ba7f6971bf4c3b2b4af261f5a3d30323ed
somehybrid <91648368+somehybrid@users.noreply.github.com>
2023-12-31 07:58:12 +0700

    Update setup.py
    Added additional metadata to setup.py
    
    Signed-off-by: somehybrid <91648368+somehybrid@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-"""
-Set up the package
-TODO: switch to pyproject.toml
-"""
-from setuptools import Extension, setup
+# TODO: switch to pyproject.toml
 
 setup(
+    name="imsocorry",
+    version="0.1.0",
+    url="https://github.com/letsbuilda/imsosorrybutinc",
+    description="Sometimes it can be necessary to call upon the ancient arts... But in C!"
     ext_modules=[
         Extension(
             name="uwuifier",
             sources=["src/uwuifier.c"],
+            include_dirs=["src"]
         ),
     ]
 )
