dcb4d3cdd386311e90b5a0311300c8f5a04212c3
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-04-06 22:24:23 -0500

    Add type stubs (#7)
    * Fixup docstrings
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Add type stubs
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Convert single file module to package
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Fix naming of extension
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Bump version
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Copy/paste docstring from extension description
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Underscore PYI like C
    
    Signed-off-by: GitHub <noreply@github.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-"""
-Set up the package
-TODO: switch to pyproject.toml
-"""
+"""Dynamic configuration for Setuptools."""
+
 from setuptools import Extension, setup
 
 setup(
     ext_modules=[
         Extension(
-            name="uwuifier",
+            name="uwuifier._uwuifier",
             include_dirs=["src"],
-            sources=["src/uwuifier.c"],
+            sources=["src/uwuifier/_uwuifier.c"],
         ),
-    ]
+    ],
 )
